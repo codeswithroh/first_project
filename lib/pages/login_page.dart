7945aa1f704ecb6d0a4f5f5c247204f9d94c8f52
codeswithroh <codeswithroh@gmail.com>
2023-02-15 18:42:31 +0530

    created a scroll view widget in login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,50 +7,55 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(children: [
-        Image.asset("assets/images/login.png", fit: BoxFit.contain),
-        const SizedBox(
-          height: 20,
-        ),
-        const Text("Welcome",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            )),
-        const SizedBox(
-          height: 20,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                    alignLabelWithHint: true,
-                    hintText: "Enter Username",
-                    labelText: "Username"),
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                    alignLabelWithHint: true,
-                    hintText: "Enter password",
-                    labelText: "Password"),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                child: const Text("Login"),
-                style: TextButton.styleFrom(),
-                onPressed: () {
-                  print('Welcome Rohit Purkait');
-                },
-              )
-            ],
+      child: SingleChildScrollView(
+        child: Column(children: [
+          Image.asset(
+            "assets/images/login.png",
+            fit: BoxFit.contain,
           ),
-        )
-      ]),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text("Welcome",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              )),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                      alignLabelWithHint: true,
+                      hintText: "Enter Username",
+                      labelText: "Username"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      alignLabelWithHint: true,
+                      hintText: "Enter password",
+                      labelText: "Password"),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  child: const Text("Login"),
+                  style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                  onPressed: () {
+                    print('Welcome Rohit Purkait');
+                  },
+                )
+              ],
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
